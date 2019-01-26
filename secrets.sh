c6b2338d70eca855410ba087da7810d85925dd68
Hackbright Student <no-reply@hackbrightacademy.com>
2019-01-25 16:46:27 -0800

    Initial commit

---
+++ secrets.sh
@@ -0,0 +1,7 @@
+# Secrets for your Eventbrite app
+# Find these at: https://www.eventbrite.ca/account-settings/apps
+# This file should never go into github!
+
+export EVENTBRITE_KEY="T266OHMAKA7QENCRNZ"
+export EVENTBRITE_SECRET="QTHYSSQBWZT6T7SVSYXTTNDUG55TC2X2ZUOOBCE6B2AQJP6LMW"
+export EVENTBRITE_TOKEN="OP7PEDEKLILXCGIRO7KC"
